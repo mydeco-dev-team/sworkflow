67b5eb144dbe14c134657ccc807343f361c5e249
Bruno Rezende <brunovianarezende@gmail.com>
2013-03-08 11:05:35 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+version = '0.1.0.dev0'
+
+setup(name='sworkflow',
+    version=version,
+    description='Simple Workflow',
+    url='https://github.com/mydeco-dev-team/sworkflow',
+    packages=['sworkflow'],
+)
