647b91457f8256fd898c1f31204f4a3b2bf16ed7
Bruno Rezende <brunovianarezende@gmail.com>
2013-03-08 11:23:02 -0300

    specify all packages provided by sworkflow

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 version = '0.1.0.dev0'
 
@@ -6,5 +6,5 @@ setup(name='sworkflow',
     version=version,
     description='Simple Workflow',
     url='https://github.com/mydeco-dev-team/sworkflow',
-    packages=['sworkflow'],
+    packages=find_packages('.'),
 )
